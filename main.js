8ba87866ec6d09895321784e901a6690f8e566c1
Tomáš Ronge <98259422+r00neey@users.noreply.github.com>
2024-10-31 20:40:53 +0100

    pridan js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-/*function show_divs() {
-    var elements = document.getElementsByClassName("item");
-    Array.prototype.forEach.call(elements, function(element) {
-        element.style.display = "block";
-    });
-}*/
-
 function toggle_divs() {
-    var elements = document.getElementsByClassName("item");
+    var elements = document.getElementsByClassName("hidden");
     Array.prototype.forEach.call(elements, function(element) {
         if (element.style.display === "block") {
             element.style.display = "none";
