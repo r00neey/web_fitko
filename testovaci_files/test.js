78b748e7d99d31acbd55bca83cca0a3662eaff1c
Tomáš Ronge <98259422+r00neey@users.noreply.github.com>
2025-02-10 22:35:46 +0100

    .

--- testovaci_files/test.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cards = document.querySelectorAll(".treneri-cards .card");
-
-    cards.forEach(function(card) {
-        card.addEventListener("click", function() {
-            card.classList.toggle("flipped");
-        });
-    });
-});
